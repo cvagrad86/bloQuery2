623132c06506458a0c277a6f5cc7230ca35c09cf
Eric Chamberlin <echamberlin@me.com>
2016-02-02 19:04:27 +0100

    Initial Commit

---
+++ BloQuery2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BloQuery2
+//
+//  Created by Eric Chamberlin on 2/2/16.
+//  Copyright © 2016 Eric Chamberlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
